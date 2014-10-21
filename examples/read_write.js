6bf55fa03a18f75d9e12755246049c4f240f736d
Santiago Gimeno <santiago.gimeno@quantion.es>
2014-10-21 10:27:33 +0200

    example: handle errors

--- examples/read_write.js
+++ examples/read_write.js
@@ -5,8 +5,20 @@ x11.createClient(function(err, display) {
     var X = display.client;
     X.InternAtom(false, '_XSETTINGS_S0', function(err, settingsOwnerAtom) {
       X.InternAtom(false, '_XSETTINGS_SETTINGS', function(err, xsettingsAtom) {
+        if (err) {
+          throw err;
+        }
+
         X.GetSelectionOwner(settingsOwnerAtom, function(err, win) {
+          if (err) {
+            throw err
+          }
+
           X.GetProperty(0, win, xsettingsAtom, 0, 0, 1e20, function(err, propValue) {
+            if (err) {
+              throw err;
+            }
+
             var decoded = xsettings.decode(propValue.data);
             console.log(decoded);
             var encoded = xsettings.encode(decoded, 0);
